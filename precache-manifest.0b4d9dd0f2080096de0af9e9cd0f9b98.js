b67d32aeb64be19453f661f958628f4c4f82ae4e
Sidhant <aintthisjokes@gmail.com>
2020-04-18 05:52:42 -0400

    Updates

---
+++ precache-manifest.0b4d9dd0f2080096de0af9e9cd0f9b98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa94db2baa095a159180c8274ea4c566",
+    "url": "/grocerline/index.html"
+  },
+  {
+    "revision": "0d4442cf1026979b0935",
+    "url": "/grocerline/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "1d755e73f0dcae81c161",
+    "url": "/grocerline/static/css/main.68b374ee.chunk.css"
+  },
+  {
+    "revision": "0d4442cf1026979b0935",
+    "url": "/grocerline/static/js/2.af52cc1e.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/grocerline/static/js/2.af52cc1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d755e73f0dcae81c161",
+    "url": "/grocerline/static/js/main.7ed9451d.chunk.js"
+  },
+  {
+    "revision": "dde41876ba001260afc3",
+    "url": "/grocerline/static/js/runtime-main.24e90108.js"
+  }
+]);
